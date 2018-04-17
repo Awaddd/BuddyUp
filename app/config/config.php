cfc9e175d8476086b766d96eef2bdcb45f5089d9
Awad <awad.dini@hotmail.co.uk>
2018-04-17 08:08:15 +0100

    db connection test

--- app/config/config.php
+++ app/config/config.php
@@ -22,10 +22,10 @@ define('SITENAME', 'BuddyUP');
 
 // PRODUCTION
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'Awad135790');
-define('DB_NAME', 'fypdb');
+define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+define('DB_USER', 'be435cb93cfdd1');
+define('DB_PASS', 'f0834500');
+define('DB_NAME', 'heroku_9a1ba8b1cb38cfb');
 
 define('URLROOT', 'https://awadini.herokuapp.com');
 
