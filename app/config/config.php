7ba5ccbbbe90fa8fa987bafc25e4370b7fe9af54
Awad <awad.dini@hotmail.co.uk>
2018-04-17 07:03:27 +0100

    second commit

--- app/config/config.php
+++ app/config/config.php
@@ -1,18 +1,32 @@
 <?php
-
+// PRODUCTION
 // DB Params
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', 'Awad135790');
+// define('DB_NAME', 'fypdb');
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', 'Awad135790');
+// define('DB_NAME', 'fypdb');
+
+// URL Root
+// define('URLROOT', 'http://localhost:1234/fypmvc');
+
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// Site name
+define('SITENAME', 'BuddyUP');
+
+// PRODUCTION
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'Awad135790');
 define('DB_NAME', 'fypdb');
 
-// App Root
-define('APPROOT', dirname(dirname(__FILE__)));
-// URL Root
-define('URLROOT', 'http://localhost:1234/fypmvc');
-
-// Site name
-define('SITENAME', 'BuddyUP');
-
+define('URLROOT', 'https://awadini.herokuapp.com/');
 
 ?>
