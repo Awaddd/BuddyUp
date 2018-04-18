c09ace625d2fa9a17cd4d8130ef520c1c713cf7c
Awad <awad.dini@hotmail.co.uk>
2018-04-18 07:08:05 +0100

    messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,6 @@
 if($("#msgbody").length > 0){
     $(document).ready(function(){
+      var chat = {}
 
       alert("OK");
 
@@ -9,6 +10,25 @@ if($("#msgbody").length > 0){
   });
 }
 
-function getMessages(){
-  alert("lol");
+chat.getMessages(){
+  alert("getMessages");
+  $.ajax({
+    url: "Messages",
+    type: "GET",
+    dataType : "json",
+  })
+
+  .done(function(data){
+    $("#chat-content").html(data);
+  })
+
+  .fail(function(xhr, status, errorThrown){
+    alert("Apologies, cannot load messages");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+  });
+
 }
+
+chat.interval = setInterval(chat.getMessages, 2000);
