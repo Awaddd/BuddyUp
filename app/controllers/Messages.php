b2b66274e27a76835e4862e53cf90980227df4c9
Awad <awad.dini@hotmail.co.uk>
2018-04-18 08:10:13 +0100

    back to messages

--- app/controllers/Messages.php
+++ app/controllers/Messages.php
@@ -49,9 +49,31 @@
       foreach ($messages as $msg) :
 
          if ($msg->Sender_ID == $user): ?>
-            <div class="message">
-              <?php echo $msg->message ?>
-            </div>
+         <!--  Sender -->
+         <div class="bubble msg__sending">
+           <div class="msg__user">
+             <?php echo $msg->Sender ?>
+             <span class="time">
+               <?php echo $date->format('H:i'); ?>
+             </span>
+           </div>
+           <div class="message">
+             <?php echo $msg->message ?>
+           </div>
+         </div>
+       <?php else: ?>
+         <!--  Receiver -->
+         <div class="bubble msg__receiving">
+           <div class="msg__user">
+             <?php echo $msg->Sender ?>
+             <span class="time">
+               <?php echo $date->format('H:i'); ?>
+             </span>
+           </div>
+           <div class="message">
+             <?php echo $msg->message ?>
+           </div>
+         </div>
           <?php
         endif;
       endforeach;
