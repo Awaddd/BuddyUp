e5206b39462258de9766c564896ba189fc8152ec
Awad <awad.dini@hotmail.co.uk>
2018-04-18 06:34:59 +0100

    messages

---
+++ public/js/chat.js
@@ -0,0 +1,23 @@
+if($("#msgbody").length > 0){
+    $(document).ready(function(){
+
+      alert("Bismillah");
+      //
+      // $.ajax({
+      //   url: "",
+      //   type: "GET",
+      //   dataType : "json",
+      // })
+      //
+      // .done(function(data){
+      //
+      // })
+      //
+      // .fail(function(xhr, status, errorThrown){
+      //   alert("Apologies, cannot load messages");
+      //   console.log("Error: " + errorThrown);
+      //   console.log("Status: " + status);
+      //   console.dir(xhr);
+      // });
+  }
+}
