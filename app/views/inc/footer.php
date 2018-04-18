33063c18d1a85ecaeb20ab1f14c6319684a34df6
Awad <awad.dini@hotmail.co.uk>
2018-04-18 07:24:30 +0100

    TEMP stopping sign up scripts

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,4 @@
-  <script src="<?php echo URLROOT; ?>/js/main.js"></script>
+  <!-- <script src="<?php echo URLROOT; ?>/js/main.js"></script> -->
   <script src="<?php echo URLROOT; ?>/js/helpers.js"></script>
   <script src="<?php echo URLROOT; ?>/js/ajax.js"></script>
   <script src="<?php echo URLROOT; ?>/js/chat.js"></script>
